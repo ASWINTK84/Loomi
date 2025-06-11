ca045fd823a690ac82c7c2665a9197ccefb4d14c
ASWINTK84 <tkaswin0911@gmail.com>
2025-06-11 11:07:39 +0530

    replaced S

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -106,7 +106,7 @@ export default function Navbar() {
                         }}
                         className="w-full text-left px-4 py-2 text-gray-700 hover:bg-indigo-100"
                       >
-                        Profile
+                        Profiles
                       </button>
                       <button
                         onClick={() => {
