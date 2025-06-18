6ab7e1de83dbeba90e427497e022a76872167055
ASWINTK84 <tkaswin0911@gmail.com>
2025-06-18 21:20:10 +0530

    looooo

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/loomi-m8pw.vercel.app/', 
+  base: '/loomi-eight.vercel.app/', 
   plugins: [
     tailwindcss(),
     react()
