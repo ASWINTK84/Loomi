3f19ca904a1c6a0128a381cb1d0351166949eead
ASWINTK84 <tkaswin0911@gmail.com>
2025-06-18 21:16:51 +0530

    fa

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fashion-store/', 
+  // base: '/fashion-store/', 
   plugins: [
     tailwindcss(),
     react()
